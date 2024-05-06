6092bccb10e86528678c0de2124337ff37a3f3da
endless-health-chain <demosmart@endlesshealthchain.com>
2024-05-06 12:50:20 +0300

    -SC

---
+++ EHC-Proxy.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "./@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+/**
+ * @title DragonTreasureProxy
+ * @author Carv
+ * @custom:security-contact security@carv.io
+ */
+contract DragonTreasureProxy is ERC1967Proxy {
+    constructor(address _logic) ERC1967Proxy(_logic, "") {}
+
+    function implementation() external view returns (address) {
+        return _implementation();
+    }
+}
